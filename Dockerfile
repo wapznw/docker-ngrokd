855764a87b7014472b03bfb183760211cafaffda
wapznw <wapznw@gmail.com>
2018-03-04 00:58:45 +0800

    add ngrokd file

--- Dockerfile
+++ Dockerfile
@@ -16,12 +16,3 @@ EXPOSE 443
 EXPOSE 4443
 CMD ["sh", "/run.sh"]
 
-docker run --restart=always -d \
--e NGROK_DOMAIN="domain" \
--v /data/domain-ssl:/ssl \
--p 80:80 \
--p 443:443 \
--p 4443:4443 \
---name container_name \
-wdj/usvn
-
--- create_domain_ssl.sh
+++ create_domain_ssl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-NGROK_DOMAIN=ngork.wendaojiang.com
+NGROK_DOMAIN=*.wendaojiang.com
 openssl genrsa -out rootCA.key 2048
 openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NGROK_DOMAIN" -days 5000 -out rootCA.pem
 openssl genrsa -out domain-ssl.key 2048
-openssl req -new -key server.key -subj "/CN=$NGROK_DOMAIN" -out server.csr
-openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out domain-ssl.crt -days 5000
+openssl req -new -key domain-ssl.key -subj "/CN=$NGROK_DOMAIN" -out domain-ssl.csr
+openssl x509 -req -in domain-ssl.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out domain-ssl.crt -days 5000
